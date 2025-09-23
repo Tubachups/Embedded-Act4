fe932f1df08fd9a26faf9b64ac77801f67ae3221
JL Tubs <johnlloydtubachups@gmail.com>
2025-09-23 08:57:09 +0800

    First commit

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("clickBtn");
+    btn.addEventListener("click", () => {
+        alert("Button Clicked!");
+    });
+});
